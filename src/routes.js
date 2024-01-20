938db35605e678a631293a3b1841403a1591794d
Natalia Arsova <130642568+NataliaArsova@users.noreply.github.com>
2024-01-20 17:46:37 +0200

    Add 404 page

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,11 @@ const homeController = require("./controllers/homeController");
 
 const movieController = require("./controllers/movieController");
 
-router.use(homeController);
 router.use(movieController);
+router.use(homeController);
+
+router.get("*", (req, res) => {
+  res.redirect("/404");
+});
 
 module.exports = router;
