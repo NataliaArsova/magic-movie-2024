1bc608ad925485be19dda6323001bcb1fb4d253b
Natalia Arsova <130642568+NataliaArsova@users.noreply.github.com>
2024-01-20 13:12:53 +0200

    Setup express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('hello');
+const express = require("express");
+
+const app = express();
+const port = 5000;
+
+app.get("/", (req, res) => {
+  res.send("Home page");
+});
+app.listen(port, () => console.log(`Server si listening on port ${port}...`));
