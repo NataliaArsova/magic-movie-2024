19d6e95c0444c9c202c508a5f3038bc357143f5c
Natalia Arsova <130642568+NataliaArsova@users.noreply.github.com>
2024-01-26 19:14:07 +0200

    Add hardcoded movie

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,5 +1,16 @@
-const movies = [];
+const movies = [
+  {
+    title: "Jungle",
+    genre: "adventure",
+    director: "Jack",
+    date: "2022",
+    imageUrl: "/image/jungle.jpeg",
+    rating: "6.7",
+    description: "great movie",
+  },
+];
 
 exports.create = (movieData) => {
+  console.log(movieData);
   movies.push(movieData);
 };
