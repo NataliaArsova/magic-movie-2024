dac419bc7c3b945e858f7891f2042e0bbdced19f
Natalia Arsova <130642568+NataliaArsova@users.noreply.github.com>
2024-01-26 18:55:16 +0200

    Add body parser

--- src/config/configExpress.js
+++ src/config/configExpress.js
@@ -2,7 +2,8 @@ const express = require("express");
 const path = require("path");
 
 function configExpress(app) {
-  app.use(express.static(path.join(__dirname, "static")));
+  app.use(express.static(path.resolve("src/static")));
+  app.use(express.urlencoded({ extended: false }));
 
   return app;
 }
